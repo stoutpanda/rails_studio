8253b4cc388424a92583d037b0657d96c1beea82
StoutPanda <jason@notacc.net>
2016-02-07 11:28:57 -0600

    Flix - TDD - Shows a movie

---
+++ crowdfund/spec/features/show_project_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+describe "Viewing an indidvidual project" do
+  it "shows a project" do
+
+    project = Project.create(name: "Torgier's Highwire Cat Extravaganza!",
+                              description: "This cat knows food and fun, but mostly just food. Cheese. Cheese. Cheese.",
+                              target_pledge_amount: 300.00,
+                              pledging_ends_on: 1.month.from_now,
+                              website: "http://project-togy.com")
+
+  visit project_url(project)
+
+
+  expect(page).to have_text(project.name)
+  expect(page).to have_text(project.description)
+  expect(page).to have_text("$300.00")
+  expect(page).to have_text(project.pledging_ends_on)
+  expect(page).to have_text(project.website)
+
+  end
+end
