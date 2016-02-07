427db5dc7e37148244f0e83774877621733fff69
StoutPanda <jason@notacc.net>
2016-02-07 12:53:57 -0600

    Flop test.

--- flix/app/models/movie.rb
+++ flix/app/models/movie.rb
@@ -1,2 +1,5 @@
 class Movie < ActiveRecord::Base
+  def flop?
+     self.total_gross < 50000000.00 || self.total_gross.blank?
+  end
 end
--- flix/spec/features/show_movie_spec.rb
+++ flix/spec/features/show_movie_spec.rb
@@ -8,10 +8,19 @@ describe "Showing an indidvidual movie" do
 
     expect(page).to have_text(movie.title)
     expect(page).to have_text(movie.rating)
-    expect(page).to have_text("$318,412,101.00")
     expect(page).to have_text(movie.description)
     expect(page).to have_text(movie.released_on)
+end
+    it "shows the total gross if the total gross exceeds $50M" do
+      movie = Movie.create(movie_attributes(total_gross: 60000000.00))
+      visit movie_url(movie)
+      expect(page).to have_text("$60,000,000.00")
+    end
+    it "shows 'Flop!' if the total_gross is less than $50M" do
+      movie = Movie.create(movie_attributes(total_gross: 49000000.00))
+      visit movie_url(movie)
+      expect(page).to have_text("Flop!")
+    end
 
 
   end
-end
