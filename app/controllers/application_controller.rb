7dc8c9c86af32e0cb324718f114a5e722f2e2d33
StoutPanda <jason@notacc.net>
2016-03-06 16:11:30 -0600

    Added admin setup, adjusted layouts and buttons as well as requiring admin login for several actions.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,4 +23,26 @@ class ApplicationController < ActionController::Base
 
     helper_method :current_user?
 
+    def require_admin
+      unless current_user_admin?
+        redirect_to root_url, alert: "Unauthorized access!"
+      end
+    end
+
+    def current_user_admin?
+      current_user && current_user.admin?
+    end
+    helper_method :current_user_admin?
+
+    def require_admin
+      unless current_user_admin?
+        redirect_to root_url, alert: "Unauthorized access!"
+    end
+    end
+
+  def current_user_admin?
+    current_user && current_user.admin?
+  end
+  
+
   end
