48fcd5c9c8a5f4a21299120015936505e315599e
StoutPanda <jason@notacc.net>
2016-03-06 16:07:23 -0600

    There is an issue with render new and when you fail to sign in correctly. Not sure why this works for the course, but once I added the admin stuff it keeps trying to take me to the session create page. I have changed it ot a redirect instead to resolve the issue.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,11 @@ class SessionsController < ApplicationController
       redirect_to(session[:intended_url] || user)
       session[:intended_url] = nil
     else
-      flash.now[:notice] = "Invalid email/password combination!"
-      render[:new]
+      flash[:notice] = "Invalid email/password combination!"
+      redirect_to new_session_url
       end
 
- end
+    end
  def destroy
    session[:user_id] = nil
    redirect_to root_url, notice: "You're now signed out!"
