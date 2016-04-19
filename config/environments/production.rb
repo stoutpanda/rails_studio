9ab4c4e952498e450685722b422e6482afa2fc0c
StoutPanda <jason@notacc.net>
2016-04-18 22:07:59 -0500

    I was mixing up what I should have been using for aws with paperclip. Changing to s3_region.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
   :storage => :s3,
   :url => ':s3_domain_url',
   :path => '/:class/:attachment/:id_partition/:style/:filename',
-  region: ENV['AWS_REGION'],
+  :s3_region => 'us-east-1'
   :s3_credentials => {
     :bucket => ENV['AWS_BUCKET'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
