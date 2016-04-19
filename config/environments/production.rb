26359f537690fab999fa8f61b5e9f14cbb5ec4b2
StoutPanda <jason@notacc.net>
2016-04-18 22:04:21 -0500

    another test for aws config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,11 +81,11 @@ Rails.application.configure do
   :storage => :s3,
   :url => ':s3_domain_url',
   :path => '/:class/:attachment/:id_partition/:style/:filename',
+  region: ENV['AWS_REGION'],
   :s3_credentials => {
     :bucket => ENV['AWS_BUCKET'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-    :region => ENV['AWS_REGION']
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
 }
 
