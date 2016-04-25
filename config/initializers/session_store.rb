c04914040630e4332a9e1a88ff64d40b78848c5f
StoutPanda <jason@notacc.net>
2016-04-24 23:36:10 -0500

    Updating server config.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :redis_store, servers: "redis://localhost:6379/0/session", key: '_flix_session'
+redis_server = ENV['REDIS_URL'] ||= 'redis://localhost:6379/0/session'
+Rails.application.config.session_store :redis_store, servers: redis_server, key: '_flix_session'
